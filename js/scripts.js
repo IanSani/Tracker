5ce52a90f55065c078d8e5bc3a756d158aa2de7c
IanSani <iansani259@gmail.com>
2019-02-15 12:37:19 +0300

    Add initial texts

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function show(){
+var inputone=parseInt(getElementById('valueone'));
+var inputtwo=parseInt(getElementById("valuetwo"));
+var inputthree=parseInt(getElementById('valuethree'));
+
+
+
+
+}
