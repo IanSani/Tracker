cf47799c9331bf7f4c648526cbac471427dee673
IanSani <iansani259@gmail.com>
2019-02-17 18:59:32 +0300

    Add changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,19 @@
 function show(){
-var inputone=parseInt(getElementById('valueone'));
-var inputtwo=parseInt(getElementById("valuetwo"));
-var inputthree=parseInt(getElementById('valuethree'));
-
-
-
-
+var inputone=parseInt(document.getElementById("valueone").value);
+var inputtwo=parseInt(document.getElementById("valuetwo").value);
+var inputthree=parseInt(document.getElementById("valuethree").value);
+if((inputone+inputtwo>inputthree) && (inputtwo+inputthree>inputone) && inputone+inputthree>inputtwo){
+if(inputone===inputtwo && inputtwo===inputthree){
+  alert("This is an Equilateral triangle");
 }
+else if(inputone===inputtwo || inputtwo===inputthree|| inputone===inputthree){
+  alert("This is an isosceles triangle")
+}
+else {
+    alert("This is a scalene triangle")
+  }
+}
+  else {
+    alert("Not a triangle")
+  }
+};
